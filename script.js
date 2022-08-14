e7e3d561fe70585810015a496a613ac85714d63c
myfriendtania <thetalentedleo2017@gmail.com>
2022-08-14 10:58:18 -0500

    fixes bugs; adds modal (pop up) and delete feature

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-$('form').on('submit', function(event) {
-    event.preventDefault();
-    var inputText = $('input[type="text"]').val()
-    $('ul').append('<li>' + inputText + '</li>')
+$('ul').on('click', '.delete-btn', function() {
+    $(this).closest('li').fadeOut(500, function() {
+        $(this).remove();
+    })
 });
+
+$('form').on('submit', function (event) {
+    event.preventDefault();
+    var inputText = $('input[type="text"]').val() //getting the value
+    if (inputText === '') return openModal('Please enter a to-do item!'); //this cancels function execution
+    $('ul').append('<li><span class ="delete-btn">❌</span>' + inputText + '</li>')
+    $('input[type="text"]').val('') //setting the value
+});
+
+function openModal(message) {
+    $('.modal').html(`
+    <p>${message}</p>
+    <a href="#" rel="modal:close">Close</a>
+    `).modal({
+        fadeDuration: 500
+    });
+}
