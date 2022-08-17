68e83946b2efed7099ce468efd11a5b5868c578c
myfriendtania <thetalentedleo2017@gmail.com>
2022-08-17 13:13:18 -0500

    added editable feature

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
-$('ul').on('click', '.delete-btn', function() {
-    $(this).closest('li').fadeOut(500, function() {
+
+// delete button
+$('ul').on('click', '.delete-btn', function () {
+    $(this).closest('li').fadeOut(500, function () {
         $(this).remove();
-    })
+    });
 });
+
+
+$('ul').on('click', '.text', function () {
+const todoText = $(this).text();
+$(this).toggleClass('text');
+$(this).html(`<input type="text" value="${todoText}"/>`)
+});
+
+
 
 $('form').on('submit', function (event) {
     event.preventDefault();
     var inputText = $('input[type="text"]').val() //getting the value
     if (inputText === '') return openModal('Please enter a to-do item!'); //this cancels function execution
-    $('ul').append('<li><span class ="delete-btn">❌</span>' + inputText + '</li>')
-    $('input[type="text"]').val('') //setting the value
+    $('ul').append(`
+    <li>
+    <span class ="delete-btn">❌</span>
+    <span class="text">${inputText}
+    </span>
+    </li>
+    `);
+    $('input[type="text"]').val(''); //setting the value
 });
 
 function openModal(message) {
